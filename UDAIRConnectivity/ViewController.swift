4cf1301a07ced2d18ef57011c98d8ebca9831f4f
Michael Jurkoic <mjjurkoic@gmail.com>
2019-12-06 01:26:23 -0600

    Basic reachability example implemented.

--- UDAIRConnectivity/ViewController.swift
+++ UDAIRConnectivity/ViewController.swift
@@ -5,16 +5,41 @@
 //  Created by Michael Jurkoic on 12/6/19.
 //  Copyright © 2019 Michael Jurkoic. All rights reserved.
 //
+//  This app is a simple example of a use of the Reachability CocoaPod.
+//  As the web host in target is a captive wifi login portal, I believe
+//  this app can only be effectively tested from an iPhone, the
+//  simulator won't be able to stand in for wifi connection testing.
+//
 
 import UIKit
+import Reachability
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var connectivityLabel: UILabel!
+    @IBOutlet weak var connectivityIndicator: UIActivityIndicatorView!
+    
+    let targetHost = "http://udair2.udallas.edu/cgi-bin/login"
+    var reachability: Reachability?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        connectivityLabel.text = "Unreachable"
+        connectivityIndicator.startAnimating()
+        setupReachability(hostName: targetHost)
     }
 
+    func setupReachability(hostName: String) {
+        self.reachability = try? Reachability(hostname: hostName)
+        self.reachability?.whenReachable = { (reachability) in
+            self.connectivityLabel.text = "Reachable"
+            self.connectivityIndicator.stopAnimating()
+        }
+        self.reachability?.whenUnreachable = { (reachability) in
+            self.connectivityLabel.text = "Unreachable"
+            self.connectivityIndicator.startAnimating()
+        }
+    }
 
 }
 
